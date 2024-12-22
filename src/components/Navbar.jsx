3d514502f63d73248ed01dba3cdfe6ecb530c304
online-rs <88519041+online-rs@users.noreply.github.com>
2024-12-22 16:36:13 +0000

    atualizar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-// Navbar.jsx
 import React, { useState } from 'react';
 import './Navbar.css';
+import logo from './sem fundo.png'; // Certifique-se de que o logo.png está no mesmo diretório ou ajuste o caminho.
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +11,11 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Afiliados</div>
+      <div className="container">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" className="navbar-logo-image" />
+        <span>Afiliados</span>
+      </div>
       <div className="navbar-hamburger" onClick={toggleMenu}>
         <div className="bar"></div>
         <div className="bar"></div>
@@ -21,6 +25,7 @@ const Navbar = () => {
         <li><a href="/">Home</a></li>
         <li><a href="/reviews">Reviews</a></li>
       </ul>
+      </div>
     </nav>
   );
 };
